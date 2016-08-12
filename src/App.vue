9b0018c69256c6cc446bdcfc123991c4086d6270
Riku Ayanokoji <riku.ayanokoji@gmail.com>
2016-08-12 16:29:26 +0800

    add dialog

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,35 @@
   <h4>Countdown Button</h4>
   <countdown-btn :interval="30" :onclick="countDownBtnClick" :onfinish="countDownFinish"></countdown-btn>
   <span>Status: {{countDownBtnStatus}}</span>
+  <hr>
+  <h4>Dialog</h4>
+  <button type="button" @click="showDialog1=true">Show</button>
+  <button type="button" @click="showDialog2=true">Show dialog with custom footer</button>
+  <dialog :title="'I\'m dialog'" :show.sync="showDialog1">
+    <template slot="content">
+      hello world.
+    </template>
+  </dialog>
+  <dialog :title="'Dialog with custom footer'" :show.sync="showDialog2">
+    <template slot="content">assign your custom footer to slot "footer".</template>
+    <div slot="footer">
+      <a href="#" class="btn btn-primary" @click.prevent="showDialog2=false">Okay, I know</a>
+      <a href="#" class="btn" @click.prevent="showDialog2=false">Close</a>
+    </div>
+  </dialog>
 </template>
 <script>
 import CountdownButton from './components/countdown-btn';
+import Dialog from './components/dialog';
 
 export default {
   data: () => ({
     countDownBtnStatus: 'available',
+    showDialog1: false,
+    showDialog2: false,
   }),
   components: {
+    Dialog,
     'countdown-btn': CountdownButton,
   },
   methods: {
@@ -25,3 +45,6 @@ export default {
   },
 };
 </script>
+<style>
+@import './app.css';
+</style>
---
+++ src/components/dialog.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="dialog-backdrop" v-if="show" @click="show=false">
+    <div class="dialog" @click.stop>
+      <div class="dialog-title">
+        <span class="dialog-close-button" @click="show=false" v-if="closeBtn">&times;</span>
+        {{title}}
+      </div>
+      <div class="dialog-content">
+        <slot name="content">{{content}}</slot>
+      </div>
+      <div class="dialog-footer text-right">
+        <slot name="footer">
+          <a href="#" class="btn btn-primary" @click.prevent="show=false">OK</a>
+        </slot>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    show: {
+      type: Boolean,
+      twoWay: true,
+    },
+    title: String,
+    content: String,
+    closeBtn: {
+      type: Boolean,
+      default: true,
+    },
+  },
+};
+</script>
+<style scoped>
+.dialog-backdrop {
+  display: flex;
+  justify-content: center;
+  -webkit-justify-content: center;
+  align-items: center;
+  -webkit-align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, .8);
+}
+.dialog {
+  background: #fff;
+  border: 1px solid #dcdcdc;
+  box-shadow: 0 2px 3px rgba(100,100,100,.27);
+  border-radius: 3px;
+  min-width: 320px;
+  max-width: 500px;
+}
+  .dialog-title, .dialog-content, .dialog-footer {
+    padding: 15px 30px;
+  }
+  .dialog-title {
+    height: 30px;
+    line-height: 30px;
+    font-size: 16px;
+    border-bottom: 1px solid #dcdcdc;
+  }
+    .dialog-close-button {
+      cursor: pointer;
+      float: right;
+      margin-right: -5px;
+      font-size: 20px;
+      height: 30px;
+      line-height: 30px;
+    }
+  .dialog-footer {
+    background: #fafafa;
+  }
+    .dialog-footer a.btn {
+      font-size: 14px;
+      margin-right: 10px;
+    }
+    .dialog-footer.text-right a.btn {
+      margin-left: 10px;
+      margin-right: 0;
+    }
+    .dialog-footer a.btn.left {
+      margin-left: 0;
+    }
+.text-right {
+  text-align: right;
+}
+</style>
